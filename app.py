814d2f3ea461406f2738f669156d009eed6aee44
mielesantos <mielesnts@gmail.com>
2024-10-09 15:05:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,15 @@ load_dotenv()
 client = OpenAI(api_key=os.environ['API_KEY'])
 
 
-response = client.chat.completions.create(
-    model='gpt-3.5-turbo',
+stream = client.chat.completions.create(
+    model='gpt-3.5-turbo',  # definindo o modelo
     messages=[{'role': 'user', 'content': 'me fale mais sobre openai'}],
+    stream=True,
 )
 
-print(response)
-print(response.choices[0].message)
+# usando stream de dados
+for chunk in stream:
+    if chunk.choices[0].delta.content is not None:
+        print(chunk.choices[0].delta.content, end='')
+
+# print(stream.choices[0].message)
