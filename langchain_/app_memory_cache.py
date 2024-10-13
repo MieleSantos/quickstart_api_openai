e984209defb71e9b79b8436ff0b3c956bf36a90a
mielesantos <mielesnts@gmail.com>
2024-10-13 16:36:57 -0300

    trabalhando com cache

---
+++ langchain_/app_memory_cache.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.globals import set_llm_cache
+from langchain_community.cache import InMemoryCache, SQLiteCache
+from langchain_openai import OpenAI
+
+load_dotenv()
+
+os.environ['OPENAi_API_KEY'] = os.environ['API_KEY']
+
+model = OpenAI()
+# usando cache na memoria
+# set_llm_cache(InMemoryCache())
+
+# usando cache no banco de dados
+set_llm_cache(SQLiteCache(database_path='openai_cache.db'))
+
+
+prompt = 'me diga quem foi Alan Turing'
+
+response1 = model.invoke(prompt)
+print(f'Chamada 1: {response1}')
+
+response2 = model.invoke(prompt)
+print(f'Chamada 2: {response2}')
